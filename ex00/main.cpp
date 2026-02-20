5168a8c4a4e91c775a1f7561f901bf05ebbe1380
sboukiou <sboukiou@student.1337.ma>
2026-02-20 04:18:53 +0000

    FEAT: setting the baseline for the implementation, data, methods...

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
+
+typedef struct data {
+	std::string date;
+	long int	value;
+} t_data;
 
 static void testInput(const int &ac, char **av, std::ifstream &file) {
 	if (ac != 2)
@@ -9,6 +15,11 @@ static void testInput(const int &ac, char **av, std::ifstream &file) {
 	if (file.is_open() == false) {
 		throw(std::runtime_error("Failed to open the file\n"));
 	}
+}
+
+t_data	parse_line(const std::string &line) {
+	t_data	data;
+	return (data);
 }
 
 int main(int ac, char **av) {
@@ -20,13 +31,11 @@ int main(int ac, char **av) {
 	}
 	catch(std::exception &e) {
 		std::cout << e.what() << std::endl;
+		std::exit(0);
 	}
 	while (file.eof() == false) {
 		std::getline(file, line);
 		content.push_back(line);
 	}
-	for (size_t i = 0; i < content.size(); i += 1)
-		std::cout << content[i] << std::endl;
-	file.close();
 	return (0);
 }
