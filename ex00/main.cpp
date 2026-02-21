8439de5a297887f55500b63e17c1457f484a5e37
sboukiou <sboukiou@student.1337.ma>
2026-02-21 01:01:02 +0000

    ADD: inclusion for the btc class

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,3 +1,4 @@
+#include "BitcoinExchange.hpp"
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -9,17 +10,10 @@ typedef struct data {
 } t_data;
 
 static void testInput(const int &ac, char **av, std::ifstream &file) {
+	(void)av;
+	(void)file;
 	if (ac != 2)
 		throw(std::runtime_error("ERR: Number of args is invalid"));
-	file.open(av[1]);
-	if (file.is_open() == false) {
-		throw(std::runtime_error("Failed to open the file\n"));
-	}
-}
-
-t_data	parse_line(const std::string &line) {
-	t_data	data;
-	return (data);
 }
 
 int main(int ac, char **av) {
@@ -33,9 +27,6 @@ int main(int ac, char **av) {
 		std::cout << e.what() << std::endl;
 		std::exit(0);
 	}
-	while (file.eof() == false) {
-		std::getline(file, line);
-		content.push_back(line);
-	}
+	Btc newRef;
 	return (0);
 }
