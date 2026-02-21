695776dc2bf60da9534610660c50787e6318cb23
sboukiou <sboukiou@student.1337.ma>
2026-02-21 01:01:24 +0000

    ADD: Definition for the Btc class

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+#ifndef BITCOIN_EXCHANGE_HPP
+# define BITCOIN_EXCHANGE_HPP
+
+#include <map>
+#include <string>
+#include <fstream>
+class Btc {
+	private:
+		std::map<std::string, double> ref;
+		std::string fileName;
+		std::ifstream	file;
+	public:
+		Btc(void);
+		Btc(const std::string &fileName);
+		Btc(const Btc &other);
+		~Btc(void);
+		Btc&	operator=(const Btc &other);
+
+
+};
+
+#endif /* BITCOIN_EXCHANGE_HPP */
