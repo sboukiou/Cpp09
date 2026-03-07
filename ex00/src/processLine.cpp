dd669de5cca0062b0c4d29c1798325a8a4ae4419
sboukiou <sboukiou@student.1337.ma>
2026-03-07 17:25:13 +0000

    UPDATE: Added the last date|value checks

--- ex00/src/processLine.cpp
+++ ex00/src/processLine.cpp
@@ -14,12 +14,19 @@ static void	parseDate(std::string &date,
 	for (size_t i = 0; i < year.length(); i += 1)
 		if (std::isdigit(year[i]) == false)
 		throw(std::runtime_error("Invalid year"));
+	if (year > "2025" || year < "2000")
+		throw(std::runtime_error("Year out of range"));
 	year = month;
 	split(month, day, year, '-');
 	trim(month);
 	trim(day);
 	if (month.length() != 2 || day.length() != 2)
 		throw(std::runtime_error("Invalid month | day"));
+	if (month > "12")
+		throw(std::runtime_error("Invalid month | day"));
+	if (month > "30")
+		throw(std::runtime_error("Invalid month | day"));
+
 	for (size_t i = 0; i < month.length(); i += 1)
 		if (std::isdigit(month[i]) == false || std::isdigit(day[i]) == false)
 		throw(std::runtime_error("Invalid month | day"));
@@ -27,18 +34,26 @@ static void	parseDate(std::string &date,
 
 static void	parseValue(std::string &value, double &hold) {
 
+	// Check if string is empty
 	if (value.empty())
 		throw(std::runtime_error("No value was found"));
+	if (value[0] == '.' || value[value.length() -1] == '.')
+		throw(std::runtime_error("Invalid dot place"));
 	std::string::const_iterator iter = value.begin();
+	++iter;
 	if (occurence(value, '.') > 1)
 		throw(std::runtime_error("Value has to many dots"));
 	while (iter != value.end() && (std::isdigit(*iter) || *iter == '.'))
 		++iter;
 	if (iter != value.end())
 		throw(std::runtime_error("Value has a non numerical char"));
+	if (value[0] == '-')
+		throw(std::runtime_error("Value is negative!"));
 	const char *temp = value.c_str();
 	char *end;
 	hold = strtod(temp, &end);
+	if (hold > 1000)
+		throw(std::runtime_error("Value too large!"));
 }
 
 void	processLine(std::map<std::string, double> &ref, std::string buffer, Type type) {
