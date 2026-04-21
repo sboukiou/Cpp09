35993fc5049eaddad1c70689243f27ba19a509f7
Felix <sboukiou@student.1337.ma>
2026-04-21 11:29:45 +0100

    FEAT: Implemented all the logic in main, using stack, TODO: split the code

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,21 +1,92 @@
-<<<<<<< HEAD
 #include <iostream>
-#include "./RPN.hpp"
+#include <stack>
+#include <stdexcept>
+#include <string>
 
-int main(void) {
-	std::cout << "Hello, World!\n";
-	testRPN();
-=======
-#include "./RPN.hpp"
+typedef enum e_op {
+	PLUS,
+	MINUS,
+	MUL,
+	DIV,
+	NIL
+}	t_op;
+
+
+t_op	is_operator(char op) {
+	if (op == '+')
+		return (PLUS);
+	if (op == '/')
+		return (DIV);
+	if (op == '*')
+		return (MUL);
+	if (op == '-')
+		return (MINUS);
+	return (NIL);
+}
+
+void	evaluate_expression(int left, int right, t_op op, std::stack<int> &yard) {
+	if (op == PLUS)
+		yard.push(left + right);
+	if (op == MINUS)
+		yard.push(left - right);
+	if (op == MUL)
+		yard.push(left * right);
+	if (op == DIV) {
+		if (right == 0)
+			throw(std::logic_error("Division by zero!"));
+		yard.push(left / right);
+	}
+}
 
 
 int main(int ac, char **av) {
+	//TODO: Parse av
+	std::stack<int> yard;
+	int	left;
+	int	right;
+	std::string expr(av[1]);
+	t_op	op;
+
 	if (ac != 2) {
-		std::cout << "Error: Numbers of Arguments is invalid!\n";
-		return (0);
+		std::cout << "ERROR: Invalid token !\n";
+		return (1);
 	}
-	std::string input = av[1];
-	evaluateExp(input);
->>>>>>> ee22b475ac2e731466f0f0922b0680a55f14bbac
+	for (size_t idx = 0; idx < expr.size(); idx += 1) {
+		if (expr[idx] != ' ') {
+			op = is_operator(expr[idx]);
+			if (op!= NIL) {
+				if (yard.empty() == true)
+					throw(std::runtime_error("Not enough tokens!"));
+				right = yard.top();
+				yard.pop();
+				if (yard.empty() == true)
+					throw(std::runtime_error("Not enough tokens!"));
+				left = yard.top();
+				yard.pop();
+				try {
+					evaluate_expression(left, right, op, yard);
+				}
+				catch (std::logic_error &e) {
+					std::cout << "ERROR: " << e.what() << std::endl;
+					return (1);
+				}
+			}
+			else if (isdigit(expr[idx])) {
+				yard.push(expr[idx] - '0');
+			}
+			else {
+				std::cout << "ERROR: Invalid token !\n";
+				return (1);
+			}
+		}
+	}
+	int result = yard.top();
+	yard.pop();
+	if (yard.empty() == false) {
+		std::cout << "ERROR: Invalid token !\n";
+		return (1);
+	}
+	std::cout << "---    " << result << "    ---\n";
+
 	return (0);
 }
