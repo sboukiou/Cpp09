404eebc3d491317534c50ab62ada3f55c301b48d
Felix <sboukiou@student.1337.ma>
2026-05-14 03:16:06 +0100

    Fixing the parsing method prototype to edit the parameter passed by ref

--- ex00/include/main.hpp
+++ ex00/include/main.hpp
@@ -11,7 +11,7 @@ void extract_line_data(std::string &line, std::map<std::string, float> &pairs);
 std::map<std::string, float> load_database(int ac, char **av);
 float parse_value(std::string &value);
 void validate_days(int year, int month, int day, std::string &keyStr);
-void parse_key_string(std::string keyStr);
+void parse_key_string(std::string &keyStr);
 void	process_input_line(std::string &line, std::map<std::string, float> &db_data);
 
 #endif /* MAIN_HPP */
